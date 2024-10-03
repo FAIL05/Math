de9b669458830be2b20dcce8720093654f78efca
Anna <annafurtado.jg019@academico.ifsul.edu.br>
2024-10-03 11:18:34 -0300

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Usuario
+
+
+admin.site.register(Usuario)
+#admin.site.register()
+
+
+
---
+++ polls/views.py
@@ -0,0 +1,24 @@
+from unittest import loader
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
+from .models import Usuario
+from django.shortcuts import get_object_or_404, render
+
+
+def index(request):
+    latest_question_list = Usuario.objects.all()
+    context = {"lastest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def login(request):
+   return render(request, 'polls/login.html')
+
+def cadastro(request):
+    return render(request, 'polls/cadastro.html')
+
+    
+
+
+
